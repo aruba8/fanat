790fa89415c3c17985bdd966399c2331774a025c
biomaks <biomaks@gmail.com>
2011-10-05 15:53:06 +0400

    Edited src/com/example/tests/AuthorizationTest.java via GitHub

--- src/com/example/tests/AuthorizationTest.java
+++ src/com/example/tests/AuthorizationTest.java
@@ -8,7 +8,7 @@ public class AuthorizationTest extends MyTestBase {
 	@Test
 	public static void testAuth(){
 		String login = "biomaks";
-		String password = "Asd123456";
+		String password = "";
 		app.getNavHelper().goToAuthPage();
 		app.getLoginHelper().login(login, password);
 		Assert.assertTrue(app.getNavHelper().returnCurrentPageAdress().contains(login));
