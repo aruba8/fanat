b4d4eeacf14bf61f9faf2f6e33725366c6e645d1
biomaks <biomaks@gmail.com>
2011-10-05 15:53:38 +0400

    Edited src/com/example/tests/SendMessages.java via GitHub

--- src/com/example/tests/SendMessages.java
+++ src/com/example/tests/SendMessages.java
@@ -10,7 +10,7 @@ public class SendMessages extends MyTestBase{
 		String subj = "test subj";
 		String message = "test message";
 		String login = "biomaks";
-		String password = "Asd123456";
+		String password = "";
 		app.getNavHelper().goToAuthPage();
 		app.getLoginHelper().login(login, password);
 		Assert.assertTrue(app.getNavHelper().returnCurrentPageAdress().contains(login));
@@ -18,7 +18,7 @@ public class SendMessages extends MyTestBase{
 		Assert.assertTrue(app.getAssertHelper().isTextPresent(login, "//div[@class='fanat-strip-profile canyon-section']"));
 		int count_before = app.getMessegesHelper().getCountOfSentedMessages();
 		app.getMessegesHelper().sendMessage(login_to, subj, message);
-		Assert.assertTrue(app.getAssertHelper().isTextPresent("Сообщение отправлено пользователю"+" "+login_to+".", "//div[@class='l-container']//div[@class='b-messages-title-container b-message-status']/p"));
+		Assert.assertTrue(app.getAssertHelper().isTextPresent("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ"+" "+login_to+".", "//div[@class='l-container']//div[@class='b-messages-title-container b-message-status']/p"));
 		int count_after = app.getMessegesHelper().getCountOfSentedMessages();
 		Assert.assertEquals(1, count_after-count_before);
 		
